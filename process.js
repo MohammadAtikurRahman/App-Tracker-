9d7f82fa5bc01a8c5fec956c73c14d23e30c1802
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-24 01:28:46 +0600

    filtering with chrome

--- process.js
+++ process.js
@@ -1,8 +1,8 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-// execute the ps command with options to list the processes of the user "atik"
-exec(`ps aux -u atik`, (err, stdout, stderr) => {
+// execute the ps command with options to list the Chrome processes of the user "atik"
+exec(`ps aux -u atik | grep chrome`, (err, stdout, stderr) => {
   if (err) {
     console.error(`Error executing ps command: ${err}`);
     return;
@@ -13,7 +13,7 @@ exec(`ps aux -u atik`, (err, stdout, stderr) => {
     const [user, pid, cpu, mem, vsz, rss, tty, stat, start, time, command] = line.trim().split(/\s+/);
     const columns = { User: user, PID: pid, CPU: cpu, Memory: mem, VSZ: vsz, RSS: rss, TTY: tty, STAT: stat, Start: start, Time: time, Command: command };
     return columns;
-  }).filter((process) => process.User === 'atik');
+  }).filter((process) => process.User === 'atik' && process.Command.includes('chrome'));
 
   // get the current date in YYYY-MM-DD format
   const currentDate = new Date().toISOString().slice(0, 10);
@@ -23,8 +23,8 @@ exec(`ps aux -u atik`, (err, stdout, stderr) => {
     return Object.values(process).join(',');
   }).join('\n');
 
-  // write the CSV data to a file named processes_{date}.csv
-  const filename = `atik_processes_${currentDate}.csv`;
+  // write the CSV data to a file named chrome_processes_{date}.csv
+  const filename = `atik_chrome_processes_${currentDate}.csv`;
   fs.writeFile(filename, Object.keys(processes[0]).join(',') + '\n' + csv, (err) => {
     if (err) {
       console.error(`Error writing to file ${filename}: ${err}`);
@@ -34,3 +34,4 @@ exec(`ps aux -u atik`, (err, stdout, stderr) => {
     console.log(csv); // log the CSV data to the console
   });
 });
+
