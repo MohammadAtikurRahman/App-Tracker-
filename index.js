22f47ed515ee85f38fb85001cf0b2c8f638952e9
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-03-13 13:56:18 +0600

    csv file totally regenerated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const os = require('os');
-const fs = require('fs');
+const os = require("os");
+const fs = require("fs");
 
 // Get the system uptime in seconds
 const uptimeInSeconds = os.uptime();
@@ -12,25 +12,30 @@ const bootTimeDate = new Date(bootTime);
 
 // Format the date with a GMT+6 timezone offset
 const options = {
-  timeZone: 'Asia/Dhaka',
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
+  timeZone: "Asia/Dhaka",
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
   hour12: true,
-
 };
 
 // Format the boot time date
-const formattedBootTime = bootTimeDate.toLocaleString('en-GB', options);
-
+const formattedBootTime = bootTimeDate
+  .toLocaleString("en-GB", options)
+  .replace(",", "") // remove the comma
+  .replace(" at", ","); // replace "at" with a comma
 // Get the current time and format it
 const now = new Date();
 
-const currentTime = now.toLocaleTimeString('en-GB',options);
+const currentTime = now
+  .toLocaleTimeString("en-GB", options)
+  .replace(",", "") // remove the comma
+  .replace(" at", ","); // replace "at" with a comma
+// Get the current time and format it
 
 // Calculate the duration since boot time in seconds
 const durationInSeconds = Math.floor((now - bootTime) / 1000);
@@ -42,27 +47,23 @@ const seconds = durationInSeconds % 60;
 
 // Format the duration as a string
 
-
 const user_pc = os.userInfo().username;
 
 const durationString = `${hours} hours ${minutes} minutes ${seconds} seconds`;
 
 // Create a string with the boot time, current time, and duration in CSV format
 
-console.log(formattedBootTime)
-console.log(currentTime)
-const csvString = `Start Day,Start Time,End Time,End Time,Total Hours Used,PC User\n${formattedBootTime},${currentTime},${durationString},${user_pc}\n`;
+console.log(formattedBootTime);
+console.log(currentTime);
+const csvString = `Start Date,Start Time,End Date,End Time,Total Hours Used,PC User\n${formattedBootTime},${currentTime},${durationString},${user_pc}\n`;
 
 // Append the CSV string to the file
-fs.appendFile('boot_time.csv', csvString, (err) => {
+fs.appendFile("boot_time.csv", csvString, (err) => {
   if (err) throw err;
-  console.log('Boot time, current time, and duration saved to boot_time.csv');
+  console.log("Boot time, current time, and duration saved to boot_time.csv");
 });
 
 // Log the duration to the console
 
-
-
-
-console.log("Who used this PC",user_pc);
+console.log("Who used this PC", user_pc);
 console.log(`Duration since boot time: ${durationString}`);
