f1947f52de67033c0550ec384a23ad7da2af860c
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-03-18 12:15:26 +0600

    user atik and media player is vlc

--- process.js
+++ process.js
@@ -1,19 +1,19 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
-// execute the ps command with options to list the Chrome processes of the user "atik"
-exec(`ps aux -u atik | grep chrome`, (err, stdout, stderr) => {
+// execute the ps command with options to list the processes for the user "atik"
+exec('ps aux | grep atik | grep vlc', (err, stdout, stderr) => {
   if (err) {
     console.error(`Error executing ps command: ${err}`);
     return;
   }
 
   // parse the output of the ps command into an array of objects
-  const processes = stdout.trim().split('\n').slice(1).map((line) => {
+  const processes = stdout.trim().split('\n').map((line) => {
     const [user, pid, cpu, mem, vsz, rss, tty, stat, start, time, command] = line.trim().split(/\s+/);
     const columns = { User: user, PID: pid, CPU: cpu, Memory: mem, VSZ: vsz, RSS: rss, TTY: tty, STAT: stat, Start: start, Time: time, Command: command };
     return columns;
-  }).filter((process) => process.User === 'atik' && process.Command.includes('chrome'));
+  });
 
   // get the current date in YYYY-MM-DD format
   const currentDate = new Date().toISOString().slice(0, 10);
@@ -23,15 +23,13 @@ exec(`ps aux -u atik | grep chrome`, (err, stdout, stderr) => {
     return Object.values(process).join(',');
   }).join('\n');
 
-  // write the CSV data to a file named chrome_processes_{date}.csv
-  const filename = `atik_chrome_processes_${currentDate}.csv`;
+  // write the CSV data to a file named atik_processes_{date}.csv
+  const filename = `atik_processes_${currentDate}.csv`;
   fs.writeFile(filename, Object.keys(processes[0]).join(',') + '\n' + csv, (err) => {
     if (err) {
       console.error(`Error writing to file ${filename}: ${err}`);
       return;
     }
     console.log(`Data written to file ${filename}`);
-    console.log(csv); // log the CSV data to the console
   });
 });
-
--- tracker.js
+++ tracker.js
@@ -1,27 +1,18 @@
-const { exec } = require('child_process');
-
-// execute the ps command with options to list all running processes
-exec(`ps aux`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Error executing ps command: ${err}`);
-    return;
+function detectVideoPlayer() {
+    const userAgent = navigator.userAgent.toLowerCase();
+  
+    if (userAgent.indexOf('vlc') !== -1) {
+      return 'VLC Media Player';
+    } else if (userAgent.indexOf('windows media player') !== -1) {
+      return 'Windows Media Player';
+    } else if (userAgent.indexOf('quicktime') !== -1) {
+      return 'QuickTime Player';
+    } else if (userAgent.indexOf('realplayer') !== -1) {
+      return 'RealPlayer';
+    } else {
+      return 'Unknown';
+    }
   }
-
-  // parse the output of the ps command into an array of objects
-  const processes = stdout.trim().split('\n').slice(1).map((line) => {
-    const [user, pid, cpu, mem, vsz, rss, tty, stat, start, time, command] = line.trim().split(/\s+/);
-    const columns = { User: user, PID: pid, CPU: cpu, Memory: mem, VSZ: vsz, RSS: rss, TTY: tty, STAT: stat, Start: start, Time: time, Command: command };
-    return columns;
-  });
-
-  // find any video player processes
-  const videoPlayers = processes.filter((process) => process.Command.includes('vlc') || process.Command.includes('mpv') || process.Command.includes('mplayer'));
-
-  // if there are video player processes, log them to the console
-  if (videoPlayers.length > 0) {
-    console.log('Video player processes:');
-    console.table(videoPlayers);
-  } else {
-    console.log('No video player processes found.');
-  }
-});
+  
+  console.log(detectVideoPlayer());
+  
